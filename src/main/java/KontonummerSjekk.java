d9a5fd2cbf44499b84156afddc38a1e89f7e93c4
Shahid Idrees <shahid.idrees@gmail.com>
2016-05-25 02:55:40 +0200

    nit

---
+++ src/main/java/KontonummerSjekk.java
@@ -0,0 +1,14 @@
+
+public class KontonummerSjekk {
+
+	public KontonummerSjekk() {
+		
+		System.out.println("constructor");
+	}
+	
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		new KontonummerSjekk();
+	}
+
+}
